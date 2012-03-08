98cb0380aa991b0bcf1a2b9c27eb52f01d8b192d
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-03-07 20:06:08 -0600

    Added in variables and while loop to the js file

--- Hernandez_Zachery_assignment2.js
+++ Hernandez_Zachery_assignment2.js
@@ -1,1 +1,23 @@
-alert("JavaScript works!");
+/* 
+Zachery Hernandez
+March 8, 2012
+Deliverable 2
+BlackBerry, Themes, Graphics
+*/
+/*
+Functions needed
+Arrays needed
+while loop needed
+for loop needed
+*/
+var devices = ["BlackBerry Bold 9700", "BlackBerry Storm 2 9550", "BlackBerry Curve 9300"],
+	rWallpaper = ["480 px by 320 px", "320 px by 480 px", "320 px by 240 px"],
+	rIcons = ["80 px by 80 px", "73 px by 73 px", "52 px by 50 px"];
+var a = "BlackBerry Theme Studio",
+	b = "import your",
+	c = "export your theme";
+var devicesLeft = 2;
+while (devicesLeft > devices.length) {
+};	
+
+alert("Designing for multiple devices.");

fa5f57e47876942a0bdbfd75970c9f838df260aa
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-03-07 21:02:36 -0600

    Rearranged console.log. Still working on while loop (not outputting correctly).

--- Hernandez_Zachery_assignment2.js
+++ Hernandez_Zachery_assignment2.js
@@ -17,22 +17,24 @@ var a = "BlackBerry Theme Studio",
 	c = "export your theme";
 var devicesLeft = 2;
 while (devicesLeft > devices.length) {
-	devicesLeft--;
 };	
 var chooseDevice = function(whatDevice) {
 	var device = devices[whatDevice],
 		rw = rWallpaper[deviceResolution],
 		ri = rIcons[deviceResolution],
+		congrats;
 		console.log("For the " + device + ", make sure your wallpaper resolution is " + rw + " and your icon resolution is " + ri + " before you can " + b + " images into the " + a + ".");
 		for (var wSize = rw; wSize === true;) {
 		};
 			console.log("Your wallpaper resolution is " + rWallpaper[whatDevice] + ", you may now " + b + " wallpapers into the " + a + ".");
-					console.log("Your icon resolution is " + rIcons[whatDevice] + ", you may now " + b + " icons into the " + a + ".");
+		for (var iSize = ri; iSize === true;) {
+		};
+			console.log("Your icon resolution is " + rIcons[whatDevice] + ", you may now " + b + " icons into the " + a + ".");
 			console.log("You may now " + c + " for the " + device + ". You have " + devicesLeft + " devices left.");
-			congrats = "You have now designed wallpapers and icons for the " + devices[0] + ", " + devices[1] + ", " + " and the " + devices[2] + ", great job!"
+			devicesLeft--;
 		return congrats;
 };
-	
+
 for (var deviceResolution = 0; deviceResolution < devices.length; deviceResolution++) {
 	var congrats = chooseDevice(deviceResolution);
 };
